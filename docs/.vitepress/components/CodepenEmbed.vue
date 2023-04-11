c7fd9ee37604d3d7237f409ebbb68b31c5afcee5
Carlos Serrano <carpasse@gmail.com>
2023-04-11 18:16:45 +0200

    feat: add Vue component to embed codepens

---
+++ docs/.vitepress/components/CodepenEmbed.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="codepen-embed">
+    <iframe
+      :src="codepenUrl"
+      :height="height"
+      :title="title"
+      frameborder="no"
+      loading="lazy"
+      allowtransparency="true"
+      allowfullscreen="true"
+      scrolling="no"
+      style="width: 100%"
+    >
+      See the Pen
+      <a :href="penUrl">{{ title }}</a> by {{ authorName }} (<a
+        :href="authorUrl"
+        >@{{ user }}</a
+      >) on <a href="https://codepen.io">CodePen</a>.
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CodepenEmbed",
+  props: {
+    user: {
+      type: String,
+      required: true,
+    },
+    pen: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: String,
+      default: "450",
+    },
+    theme: {
+      type: String,
+      default: "dark",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    authorName: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    codepenUrl() {
+      return `https://codepen.io/${this.user}/embed/${this.pen}?height=${this.height}&theme-id=${this.theme}&default-tab=result`;
+    },
+    authorUrl() {
+      return `https://codepen.io/${this.user}`;
+    },
+    penUrl() {
+      return `https://codepen.io/${this.user}/pen/${this.pen}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.codepen-embed {
+  margin: 0 auto;
+  width: 95%;
+}
+</style>
